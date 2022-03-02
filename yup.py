927a6b453593ba37adcfa81bf44bc1d3f4d63efd
nooralam15 <55167857+nooralam15@users.noreply.github.com>
2022-03-02 14:34:41 -0700

    Update yup.py

--- yup.py
+++ yup.py
@@ -5,15 +5,29 @@ words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 #Initialize a function that will sort the array using the bubble sort alg
 def bubbleSort(anArray):
     for x in range(len(anArray)-1): 
-        count = 1
-        for i in range(len(anArray)-count):
+        for i in range(x+1):
             if anArray[i] > anArray[i + 1]:
                 temp = anArray[i]
-                temp2 = anArray[i + 1]
-                anArray[i] = temp2
+                anArray[i] = anArray[i + 1]
                 anArray[i + 1] = temp
-                count+=1
 
- 
-print(bubbleSort(nums))
-print(bubbleSort(words))
+# #Initialize a function that will sort the array using the bubble sort alg
+# def bubbleSort(anArray):
+#     for x in range(len(anArray)-1): 
+#         count = 1
+#         for i in range(len(anArray)-count):
+#             if anArray[i] > anArray[i + 1]:
+#                 temp = anArray[i]
+#                 temp2 = anArray[i + 1]
+#                 anArray[i] = temp2
+#                 anArray[i + 1] = temp
+#                 count+=1
+
+ #invoke functions
+bubbleSort(nums)
+bubbleSort(words)
+
+
+#print results
+print(nums)
+print(words)
