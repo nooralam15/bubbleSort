302057a002d052ec878a333d36504e712d7abab1
nooralam15 <55167857+nooralam15@users.noreply.github.com>
2022-03-02 14:04:26 -0700

    Create yup.py

---
+++ yup.py
@@ -0,0 +1,19 @@
+nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
+words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
+
+
+#Initialize a function that will sort the array using the bubble sort alg
+def bubbleSort(anArray):
+    for x in range(len(anArray)-1): 
+        count = 1
+        for i in range(len(anArray)-count):
+            if anArray[i] > anArray[i + 1]:
+                temp = anArray[i]
+                temp2 = anArray[i + 1]
+                anArray[i] = temp2
+                anArray[i + 1] = temp
+                count+=1
+
+ 
+print(bubbleSort(nums))
+print(bubbleSort(words))
