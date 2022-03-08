5b0a5034115e0f6966b4019927448a3d6569cd36
nooralam15 <nooralam14@icloud.com>
2022-03-08 14:39:56 -0700

    Update yup.py

--- yup.py
+++ yup.py
@@ -3,11 +3,11 @@ words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
 
 def selectionSort(anArray):
-    for fillSlot in range(len(anArray)-1, 0, -1):
+    for fillSlot in range(0, len(anArray)-2):
         #Search for minimum
         minPosition = fillSlot
-        for i in range(fillSlot):
-            if anArray[i] > anArray[minPosition]:
+        for i in range(fillSlot + 1, len(anArray)-1):
+            if anArray[i] < anArray[minPosition]:
                 minPosition = i
         
         temp = anArray[fillSlot]
@@ -17,6 +17,8 @@ def selectionSort(anArray):
 
 #invoke functions
 selectionSort(nums)
+selectionSort(words)
+
 
 #print results
 print(nums)
