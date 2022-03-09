ec8d03150af69dadab66faeee542e2fcba3df1f3
nooralam15 <nooralam14@icloud.com>
2022-03-09 14:50:55 -0700

    Update yup.py

--- yup.py
+++ yup.py
@@ -2,24 +2,10 @@ nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
 words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
 
-def selectionSort(anArray):
-    for fillSlot in range(0, len(anArray)-2):
-        #Search for minimum
-        minPosition = fillSlot
-        for i in range(fillSlot + 1, len(anArray)-1):
-            if anArray[i] < anArray[minPosition]:
-                minPosition = i
-        
-        temp = anArray[fillSlot]
-        anArray[fillSlot] = anArray[minPosition]
-        anArray[minPosition] = temp
+def insertionSort(anArray):
+    for insertPos in range(1, len(anArray)):
+        insertVal = anArray[insertPos]
 
+        while anArray[insertPos - 1] > insertVal:
+            
 
-#invoke functions
-selectionSort(nums)
-selectionSort(words)
-
-
-#print results
-print(nums)
-print(words)
